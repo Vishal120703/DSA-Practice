2a656a77829490fe45ab1d9e30caf367119e6894
vishal gupta <vishalgupta120703@gmail.com>
2026-02-24 12:58:26 +0530

    i am learning trees

---
+++ DSA_Practice/src/Trees/creation.java
@@ -0,0 +1,50 @@
+package Trees;
+
+public class creation {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void print(Node root){
+
+        if(root == null){
+            return;
+        }
+
+        print(root.left);
+
+        print(root.right);
+
+
+    }
+    public static int countElement(Node root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null ){
+            return root.data;
+        }
+        return countElement(root.left)+ countElement(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(50);
+
+        root.left = new Node(30);
+        root.right = new Node(70);
+
+        root.left.left = new Node(20);
+        root.left.right = new Node(40);
+
+        root.right.left = new Node(60);
+        root.right.right = new Node(80);
+//        print(root);
+        System.out.println(countElement(root));;
+    }
+}
